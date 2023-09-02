08f90018461ff8b49d48cc7e2cd9ecf0c22caaff
Layla Ventilari <laylaventilari5@gmail.com>
2023-09-01 21:38:24 -0300

    feat: create carrousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,25 @@
 import Image from "next/image"
-import { styled } from "../styles"
+
+import { useKeenSlider } from 'keen-slider/react'
+
 import { HomeContainer, Product } from "../styles/pages/home"
 
 import shirt1 from '../assets/camisetas/1.png'
 import shirt2 from '../assets/camisetas/2.png'
 import shirt3 from '../assets/camisetas/3.png'
 
+import 'keen-slider/keen-slider.min.css';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
   return (
-   <HomeContainer>
-      <Product>
+   <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={480} alt=""/>
 
         <footer>
@@ -19,8 +28,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span> R$ 79,00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span> R$ 79,00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt1} width={520} height={480} alt=""/>
 
         <footer>
           <strong>Camiseta X</strong>
